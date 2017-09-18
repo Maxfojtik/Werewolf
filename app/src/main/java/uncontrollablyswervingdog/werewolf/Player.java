e75429901e9445653aa80da713f0fb7dbb5f1e3a
Matthew <matthew.d.zirbel@gmail.com>
2017-09-17 21:06:20 -0400

    Working on done button, not complete

---
+++ app/src/main/java/uncontrollablyswervingdog/werewolf/Player.java
@@ -0,0 +1,16 @@
+package uncontrollablyswervingdog.werewolf;
+
+/**
+ * Created by matth on 9/16/2017.
+ */
+
+public class Player {
+    String name;
+    String role;
+    String action;
+    String result;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
